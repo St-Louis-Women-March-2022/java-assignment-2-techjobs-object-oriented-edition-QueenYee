d51ae07261bc793709666c7c0b189d562fbeb428
Stanyale Riley <freedomside@yahoo.com>
2022-10-05 17:02:10 -0500

    Initial commit

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -11,6 +13,7 @@ public class PositionType {
         nextId++;
     }
 
+    @Override
     public PositionType(String value) {
         this();
         this.value = value;
@@ -20,6 +23,19 @@ public class PositionType {
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
 
     // Getters and Setters:
 
